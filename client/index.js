192e29af5caa2ee4a7ad1df29cf06250195e255d
kyrie irving <kyrie@kyries-MacBook-Pro.local>
2018-08-07 13:57:33 -0500

    uses graphQL

---
+++ client/index.js
@@ -0,0 +1,39 @@
+import './style/style.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router, Route, hashHistory, IndexRoute } from 'react-router';
+import ApolloClient from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
+
+import App from './components/App';
+import SongList from './components/SongList';
+import SongCreate from './components/SongCreate';
+import SongDetail from './components/SongDetail';
+
+
+//takes every data that is fetched by apollo
+//client and runs it through o => o.id
+//whatever is returned by the function
+//is used to identify the data in the store
+const client = new ApolloClient({
+  dataIdFromObject: o => o.id
+});
+
+const Root = () => {
+  return (
+  	<ApolloProvider client={client}>
+  		<Router history={hashHistory}>
+  			<Route path="/" component={App}>
+  				<IndexRoute component={SongList} />
+  				<Route path="songs/new" component={SongCreate} />
+          <Route path="songs/:id" component={SongDetail} />
+  			</Route>
+  		</Router>
+  	</ApolloProvider>
+  );
+};
+
+ReactDOM.render(
+  <Root />,
+  document.querySelector('#root')
+);
